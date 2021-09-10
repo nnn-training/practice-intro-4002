f410d6486d87b8bbb372d1fb18e0f34424408fa3
piyokichi <yurika.1212.8912@gmail.com>
2021-09-10 10:20:41 +0900

    /photos で :title パラメータを受け取るように変更

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,16 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/1', (req, res, next) => {
+  res.render('photos');
+});
+
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
+});
+
+router.get('/hoge', (req, res, next) => {
+  res.send(req.params.hoge);
+});
+
 module.exports = router;
