d5411a5d0639fae138dd2cf4ab59c1b304100537
kinoa kanawashi <kanawashi@kinoanoMacBook-Air.local>
2021-10-01 14:55:48 +0900

    /photo.jsで :title パラメーターを受け取れるよう変更

--- routes/photos.js
+++ routes/photos.js
@@ -1,9 +1,20 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
 
 router.get('/', (req, res, next) => {
   res.send('Some photos');
+});
+
+router.get('/:title', (req, res, next) => {
+  const title = req.params.title;
+  // The following IFcheck was written roughly, so this does NOT guarntee to protect XSS attack.
+  if(title.indexOf('<') !== -1 || title.indexOf('>') !== -1) {
+    next(createError(404));
+    return;
+  }
+  res.send(title);
 });
 
 module.exports = router;
