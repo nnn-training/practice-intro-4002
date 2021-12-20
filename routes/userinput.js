e5ac2483037baaab6c0befe4ff36f8dace315bb4
BlueZhiaar <tightblackmachine@gmail.com>
2021-12-20 13:44:51 +0900

    /photosで:titleパラメータを受け取るように変更

--- routes/userinput.js
+++ routes/userinput.js
@@ -2,11 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
-});
-
-router.get('/:id',(req, res, next) => {
+router.get('/:id', (req, res, next) => {
   res.send(req.params.id);
 });
 
