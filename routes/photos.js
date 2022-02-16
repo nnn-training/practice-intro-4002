251e88e1ecd49a59881f0373763e5073f62bf8f6
hana fleur <yukay2184@gmail.com>
2022-02-16 14:08:56 +0900

    photos/ でURLパラメータを受け取って表示するよう修正

--- routes/photos.js
+++ routes/photos.js
@@ -5,5 +5,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
-
+//URL内に含まれた :id という部分にあたる文字列を、ページ内に表示させる
+router.get('/:id', (req, res, next) => {
+  res.send(req.params.id);
+})
 module.exports = router;
