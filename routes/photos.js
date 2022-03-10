e5fdd531f6f32a03f0c87caa01e288ff7dfa7339
kobucha <kobucha>
2022-03-10 18:26:19 +0900

    パラメーターを受け取り表示しました

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,8 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/:id', (req, res, next) => {
+  res.send(`パスのパラメーター： <strong>${req.params.id}</strong>`);
+});
+
 module.exports = router;
