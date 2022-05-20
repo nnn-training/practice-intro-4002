63c1064ce3f1072ee2c03bec375efed366ad010b
Kouhei Hamano <marco.fronsys@gmail.com>
2022-05-20 23:50:30 +0900

    /photos/:titleの:titleにパラメータを投入したものが画面に表示されるよう実装

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,16 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/:title', (req, res, next) => {
+  res.send(
+    req.params.title
+    // XSS対策のreplaceを追加(たぶんこういうやり方ではないかも)
+    .replace(/&/g, '&amp')
+    .replace(/</g, '&lt')
+    .replace(/>/g, '&gt')
+    .replace(/"/g, '&quot')
+    .replace(/'/g, '&#x27')
+    );
+  });
+
 module.exports = router;
