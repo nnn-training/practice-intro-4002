e00f094424b633b9ff299fb2a88d0ffeb05b1cb6
Fuuchin <denkisutandokese@outlook.jp>
2022-12-20 20:07:00 +0900

    :titleの部分をそもまま文字列をsendするようにした

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+router.get('/nyobi', (req, res, next) => {
+  res.send('N予備校！すごい！本当にすごいんだ！');
+});
+
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
+});
+
 module.exports = router;
