1f0d8b7de135cca06aa5095f103283d973745710
abc <chihiro.hoya@gmail.com>
2023-06-27 11:18:36 +0900

    URLに入力された文字をwebページに表示

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:title', (req, res, next) => {
+  res.send(req.params.title);
+});
+
+
 module.exports = router;
